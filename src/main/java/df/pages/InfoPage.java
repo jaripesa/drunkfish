d0a9a8cf3d45aa38576a7e4b14ba1e498e1b8580
Jaripekka Salminen <ext-jaripekka.salminen@nokia.com>
2014-05-10 18:41:12 +0300

    Authentication in progress

--- src/main/java/df/pages/InfoPage.java
+++ src/main/java/df/pages/InfoPage.java
@@ -1,5 +1,13 @@
 package df.pages;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Map;
 
@@ -13,7 +21,52 @@ public class InfoPage extends Page
     {
         begin(w, "Info Page");
 
-        String code = params.get("code");
+        String error = params.get("error");
+
+        if (error != null) {
+            out(w, "Error " + error);
+
+        } else {
+            final String code = params.get("code");
+            final HttpClient client = HttpClientBuilder.create().build();
+
+            final String url = "https://api.moves-app.com/oauth/v1/access_token" +
+                "?grant_type=authorization_code&" +
+                "code=" + encode(code) + "&" +
+                "client_id=" + encode(CLIENT_ID) + "&" +
+                "client_secret=" + encode(CLIENT_SECRET) + "&" +
+                "redirect_uri=" + encode(HELP_PAGE_URL);
+
+            System.err.println(url);
+
+            final HttpPost post = new HttpPost(url);
+
+            try {
+                final HttpResponse response = client.execute(post);
+                out(w, "Response Code : " + response.getStatusLine().getStatusCode());
+                System.err.println(response.getStatusLine().getStatusCode());
+
+                BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    System.err.println(line);
+                }
+
+                /*
+                 * invalid_grant: the provided authorization code is not valid,
+                 * has been revoked or has expired (after 5 minutes).
+                 * Note that both successful and unsuccessful requests revoke the code.
+                 *
+                 */
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         end(w);
     }
--- src/main/java/df/pages/Page.java
+++ src/main/java/df/pages/Page.java
@@ -2,10 +2,18 @@ package df.pages;
 
 
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 public abstract class Page
 {
+    static final String CLIENT_ID = "e1Wy2Hc_o0k18d7ry1f4qrJefT2jQU9Q";
+    static final String CLIENT_SECRET = "u8U8_C_0nw69E35gP2S2Z4x6sh1ceNcJgk5hBLCItXuQdGHS6IVKOMWBpmHLLw1b";
+    static final String INFO_PAGE_URL = "http://medios.fi/df/info";
+    static final String HELP_PAGE_URL = "http://medios.fi/df/help";
+    static final String SCOPE = "activity location";
+
 
     public abstract void view(PrintWriter writer,
                               Map<String, String> params);
@@ -29,6 +37,18 @@ public abstract class Page
     protected void end(final PrintWriter w)
     {
         out(w, "</body></html>");
+    }
+
+
+    protected String encode(final String string)
+    {
+        try {
+            return URLEncoder.encode(string, "UTF-8");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return string;
+        }
     }
 
 }
